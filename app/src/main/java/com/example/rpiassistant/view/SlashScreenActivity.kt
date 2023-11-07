709d7657a4ecca75a18ba0625c5d0ee42dd7510c
Sakibur Rahman <sakibur521@gmail.com>
2023-11-07 13:09:13 +0600

    updateed

--- app/src/main/java/com/example/rpiassistant/view/SlashScreenActivity.kt
+++ app/src/main/java/com/example/rpiassistant/view/SlashScreenActivity.kt
@@ -21,37 +21,6 @@ class SlashScreenActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        binding.animationView.playAnimation()
-        binding.animationView.addAnimatorListener(object : Animator.AnimatorListener{
-            override fun onAnimationStart(p0: Animator) {
-
-            }
-
-            override fun onAnimationEnd(p0: Animator) {
-                startActivity(Intent(this@SlashScreenActivity , MainActivity::class.java))
-                finish()
-            }
-
-            override fun onAnimationCancel(p0: Animator) {
-
-            }
-
-            override fun onAnimationRepeat(p0: Animator) {
-
-            }
-        })
-
-
-
-
-        if (isInternetAvailable(this)){
-
-
-
-        }else{
-            Toast.makeText(this , "No Internet Connection" , Toast.LENGTH_SHORT).show()
-
-        }
 
 
 
@@ -61,16 +30,7 @@ class SlashScreenActivity : AppCompatActivity() {
 
 
 
-    fun isInternetAvailable(context: Context): Boolean {
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-        val network = connectivityManager.activeNetwork
-        val capabilities = connectivityManager.getNetworkCapabilities(network)
-        return (capabilities != null) && (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || capabilities.hasTransport(
-            NetworkCapabilities.TRANSPORT_CELLULAR
-        ))
-    }
 
 
 }
